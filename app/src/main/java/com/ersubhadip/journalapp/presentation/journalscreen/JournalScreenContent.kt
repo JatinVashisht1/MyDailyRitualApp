a83df7473a15878535ac50fe1aa4d29723bf4f54
JatinVashisht1 <jatin8085vashisht@gmail.com>
2024-01-08 11:32:38 +0530

    chore(experimenting different ways): trying different ways to style text better

--- app/src/main/java/com/ersubhadip/journalapp/presentation/journalscreen/JournalScreenContent.kt
+++ app/src/main/java/com/ersubhadip/journalapp/presentation/journalscreen/JournalScreenContent.kt
@@ -1,27 +1,42 @@
 package com.ersubhadip.journalapp.presentation.journalscreen
 
+import android.graphics.Typeface
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.ersubhadip.journalapp.presentation.journalscreen.components.JournalBodyTextField
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.text.HtmlCompat
 import com.ersubhadip.journalapp.presentation.journalscreen.components.JournalTitleTextField
 import com.ersubhadip.journalapp.presentation.journalscreen.uistate.JournalScreenState
 import com.ersubhadip.journalapp.presentation.utils.MyPaddingValues
 import com.mohamedrejeb.richeditor.model.RichTextState
+import com.mohamedrejeb.richeditor.ui.BasicRichText
 import com.mohamedrejeb.richeditor.ui.material3.RichTextEditor
-import com.mohamedrejeb.richeditor.ui.material3.RichTextEditorColors
-import com.mohamedrejeb.richeditor.ui.material3.RichTextEditorDefaults
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -53,6 +68,41 @@ fun JournalScreenContent(
                 label = { Text(text = journalScreenState.journalBodyTextFieldState.label) },
                 placeholder = { Text(text = journalScreenState.journalBodyTextFieldState.placeholder) },
             )
+
+            Spacer(modifier = Modifier.height(MyPaddingValues.MEDIUM))
+
+//            BasicRichText(
+//                state = richTextEditorState,
+//                modifier = Modifier.size(100.dp),
+//                style = MaterialTheme.typography.titleMedium.copy(
+//                    color = Color.Yellow,
+//                    fontSize = MaterialTheme.typography.displayLarge.fontSize,
+//                ),
+//            )
+
+        }
+    }
+}
+
+fun Spanned.toAnnotatedString(): AnnotatedString = buildAnnotatedString {
+    val spanned = this@toAnnotatedString
+    append(spanned.toString())
+    getSpans(0, spanned.length, Any::class.java)
+    val start = getSpanStart(spanned)
+    val end = getSpanEnd(spanned.length)
+
+    getSpans(0, spanned.length, Any::class.java).forEach { span ->
+        val start = getSpanStart(span)
+        val end = getSpanEnd(span)
+        when (span) {
+            is StyleSpan -> when (span.style) {
+
+                Typeface.BOLD -> addStyle(SpanStyle(fontWeight = FontWeight.Bold), start, end)
+                Typeface.ITALIC -> addStyle(SpanStyle(fontStyle = FontStyle.Italic), start, end)
+                Typeface.BOLD_ITALIC -> addStyle(SpanStyle(fontWeight = FontWeight.Bold, fontStyle = FontStyle.Italic), start, end)
+            }
+            is UnderlineSpan -> addStyle(SpanStyle(textDecoration = TextDecoration.Underline), start, end)
+            is ForegroundColorSpan -> addStyle(SpanStyle(color = Color(span.foregroundColor)), start, end)
         }
     }
 }
